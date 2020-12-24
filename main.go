d0b2b45fd35c4eea72a2b74502506398de739ae0
Tamal Saha <tamal@appscode.com>
2020-12-24 14:39:21 -0800

    added-all

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-lib "kmodules.xyz/schema-checker"
 	"go.bytebuilders.dev/ui-wizards/apis/wizards/v1alpha1"
+	lib "kmodules.xyz/schema-checker"
 )
 
 func main() {
 	checker := lib.New(
 		"/home/tamal/go/src/go.bytebuilders.dev/ui-wizards",
 		[]interface{}{
-		v1alpha1.IdentityServerSpec{},
-	})
+			v1alpha1.IdentityServerSpec{},
+		})
 
 	result, err := checker.CheckAll()
 	if err != nil {
